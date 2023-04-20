5b120a9d4f572290aa863e22875ef5973219d9bc
Brian Arnold <brianjohnarnold@gmail.com>
2023-04-20 13:56:25 -0400

    put data processing and rescaling into function

--- src/spatialnn/__main__.py
+++ src/spatialnn/__main__.py
@@ -28,28 +28,41 @@ def set_seeds(seed):
     torch.backends.cudnn.benchmark = False
     torch.backends.cudnn.deterministic = True
 
+def load_rescale_input_data(args):
+  coords = np.loadtxt(args.input_layer, delimiter='\t')
+  expression = np.loadtxt(args.output_layer, delimiter='\t')
+  assert coords.shape[0] == expression.shape[0], 'Input and output files do not have same number of rows! Some spots are missing or do not have expression PC values!'
+  
+  scaler = preprocessing.StandardScaler().fit(expression)
+  A_scaled = scaler.transform(expression)
+  A_torch = torch.tensor(A_scaled,dtype=torch.float32)
+
+  scaler = preprocessing.StandardScaler().fit(coords)
+  S_scaled = scaler.transform(coords)
+  S_torch = torch.tensor(S_scaled,dtype=torch.float32)
+
+  return S_torch, A_torch
+
 def main():
 
   set_seeds(args.seed)
   os.makedirs(args.output_dir, exist_ok=True) 
 
-  # Load input data
-  coords = np.loadtxt(args.input_layer, delimiter='\t')
-  expression = np.loadtxt(args.output_layer, delimiter='\t')
-  assert coords.shape[0] == expression.shape[0], 'Input and output files do not have same number of rows! Some spots are missing or do not have expression PC values!'
-
+  # Load input data and rescale, S_torch and A_torch are torch tensors representing the spatial coordinates and expression data, respectively
+  S_torch, A_torch = load_rescale_input_data(args)
+  
   # rescale input data
-  scaler = preprocessing.StandardScaler().fit(expression)
+  """scaler = preprocessing.StandardScaler().fit(expression)
   A_scaled = scaler.transform(expression)
   A_torch=torch.tensor(A_scaled,dtype=torch.float32)
 
   scaler = preprocessing.StandardScaler().fit(coords)
   S_scaled = scaler.transform(coords)
-  S_torch=torch.tensor(S_scaled,dtype=torch.float32)
+  S_torch=torch.tensor(S_scaled,dtype=torch.float32)"""
 
   # train neural network
   mod, loss_list = train(S_torch, A_torch,
-                          S_hidden_list=[args.hidden_units_spatial]*2, A_hidden_list=[10], 
+                          S_hidden_list=[args.hidden_units_spatial], A_hidden_list=[10], 
                           epochs=args.epochs, checkpoint=args.checkpoint, 
                           save_dir=args.output_dir, optim=args.optimizer, seed=args.seed) 
 
