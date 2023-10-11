c6dad1e735fb5122a2aff6de20b0b23faeac1f82
hirakarkar <hsarkar@cs.stonybrook.edu>
2023-10-11 00:25:12 -0400

    added images

--- docs/conf.py
+++ docs/conf.py
@@ -171,7 +171,7 @@ linkcheck_ignore = [
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
-html_logo = "_static/img/raphael-lab-logo.png"
+html_logo = "_static/img/gaston_logo.png"
 html_theme_options = {"navigation_depth": 4, "logo_only": True}
 html_show_sphinx = False
 
