beef53c22b5e57e0112de8921e1dc99a11c89793
uthsavc <uchitra@wash.cs.princeton.edu>
2024-04-05 01:44:42 -0400

    add GLM-PC and PC options, add model selection

--- src/gaston/parse_adata.py
+++ src/gaston/parse_adata.py
@@ -46,4 +46,23 @@ def get_gaston_input_adata(data_folder, get_rgb=False, spot_umi_threshold=50):
     RGB_mean = RGB_mean[RGB_mean.index.isin(df_pos['barcode'])]
 
     return counts_mat, coords_mat, gene_labels, RGB_mean.to_numpy()
+
+def get_top_pearson_residuals(num_pcs, counts_mat, coords_mat, gene_labels=None, n_top_genes=5000, clip=0.01):
+    df=pd.DataFrame(counts_mat, columns=gene_labels)
+    adata=sc.AnnData(df)
+    adata.obsm["coords"] = coords_mat
     
+    sc.experimental.pp.highly_variable_genes(
+            adata, flavor="pearson_residuals", n_top_genes=n_top_genes
+    )
+    
+    adata = adata[:, adata.var["highly_variable"]]
+    adata.layers["raw"] = adata.X.copy()
+    adata.layers["sqrt_norm"] = np.sqrt(
+        sc.pp.normalize_total(adata, inplace=False)["X"]
+    )
+    
+    theta=np.Inf
+    sc.experimental.pp.normalize_pearson_residuals(adata, clip=clip, theta=theta)
+    sc.pp.pca(adata, n_comps=num_pcs)
+    return adata.obsm['X_pca']
